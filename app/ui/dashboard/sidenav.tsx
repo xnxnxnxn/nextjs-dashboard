03afa4858044ee9882f0655428b620f2886ac391
xinning <574431874@qq.com>
2024-08-16 16:54:52 +0800

    学完

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
 import AcmeLogo from "@/app/ui/acme-logo";
 import { PowerIcon } from "@heroicons/react/24/outline";
+import { logout } from "@/app/lib/actions";
+import { useActionState } from "react";
 
 export default function SideNav() {
+  const [errorMessage, formAction, isPending] = useActionState(logout, undefined);
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40" href="/">
@@ -15,8 +18,11 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+        <form action={formAction}>
+          <button
+            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            aria-disabled={isPending}
+          >
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
